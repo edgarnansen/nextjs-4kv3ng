98dbe0aa0ce5c6347332a06cbc897e66753d188c
Anders Engan <anders.engan@gmail.com>
2021-09-29 01:59:20 +0200

    Prefer axios

--- pages/api/integration/currencies.ts
+++ pages/api/integration/currencies.ts
@@ -1,8 +1,8 @@
+import axios from 'axios';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { externalAPIBase } from '../../../utils/api';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const response = await fetch(`${externalAPIBase}/currencies`);
-  const result = await response.json();
-  res.status(200).json(result);
+  const response = await axios.get(`${externalAPIBase}/currencies`);
+  res.status(200).json(response.data);
 };
