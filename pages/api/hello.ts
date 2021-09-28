5967420eaf70e94d9f39e086fa0333916fb9c682
Anders Engan <anders.engan@gmail.com>
2021-09-28 12:10:49 +0200

    Add prettier

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
   // Open Chrome DevTools to step through the debugger!
