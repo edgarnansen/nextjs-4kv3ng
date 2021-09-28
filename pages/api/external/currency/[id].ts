24e491c5889ceb83ca0738a28aca871489137eab
Anders Engan <anders.engan@gmail.com>
2021-09-28 15:50:37 +0200

    Randomly slow and fail

--- pages/api/external/currency/[id].ts
+++ pages/api/external/currency/[id].ts
@@ -3,7 +3,55 @@ import { currencies } from '../../../../utils/currency';
 import { promises as fs } from 'fs';
 import path from 'path';
 
+const publicFolder = './public';
 const loremFile = 'lorem.txt';
+
+const randomFailureRatePercentage = 10;
+const shouldFailRandomly = () => {
+  const randomPercentage = getRandomNumber(0, 100);
+  return randomPercentage < randomFailureRatePercentage;
+};
+
+const getRandomNumber = (min: number, max: number) => {
+  return Math.random() * (max - min) + min;
+};
+
+const randomSlownessRatePercentage = 25;
+const responseShouldBeSlow = () => {
+  const randomPercentage = getRandomNumber(0, 100);
+  return randomPercentage < randomSlownessRatePercentage;
+};
+
+const slowBy = (ms: number) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+};
+
+enum SlowNess {
+  A_BIT = 300,
+  A_LITTLE = 600,
+  A_GREAT_DEAL = 1_000,
+  A_LOT = 10_000,
+  TOO_MUCH = 30_000,
+}
+
+const randomSlowness = async () => {
+  const randomPercentage = getRandomNumber(0, 100);
+  if (randomPercentage < 50) {
+    return slowBy(SlowNess.A_BIT);
+  }
+  if (randomPercentage < 65) {
+    return slowBy(SlowNess.A_LITTLE);
+  }
+  if (randomPercentage < 80) {
+    return slowBy(SlowNess.A_GREAT_DEAL);
+  }
+  if (randomPercentage < 95) {
+    return slowBy(SlowNess.A_LOT);
+  }
+  return slowBy(SlowNess.TOO_MUCH);
+};
 
 export default async function currencyHandler(
   req: NextApiRequest,
@@ -19,6 +67,14 @@ export default async function currencyHandler(
     return res.status(405).end(`Method ${method} Not Allowed`);
   }
 
+  if (shouldFailRandomly()) {
+    return res.status(500).end('Internal Server Error');
+  }
+
+  if (responseShouldBeSlow()) {
+    await randomSlowness();
+  }
+
   const currency = currencies.find(
     ({ alphabeticCode }) => id === alphabeticCode
   );
@@ -28,7 +84,7 @@ export default async function currencyHandler(
   }
 
   const lorem = await fs.readFile(
-    path.join(path.resolve('./public'), loremFile),
+    path.join(path.resolve(publicFolder), loremFile),
     'utf-8'
   );
 
--- utils/currency/index.ts
+++ utils/currency/index.ts
@@ -2,46 +2,139 @@ export enum CurrencyCode {
   BHD = 'BHD',
   EUR = 'EUR',
   GBP = 'GBP',
+  JPY = 'JPY',
   NOK = 'NOK',
   USD = 'USD',
 }
 
+export enum CountryCode {
+  BH = 'Bahrain',
+  GB_ENG = 'England',
+  NO = 'Norway',
+  JP = 'Japan',
+  US = 'United States',
+}
+
+type ConversionRates = {
+  [code in keyof typeof CurrencyCode]: number;
+};
+
 export interface Currency {
   alphabeticCode: CurrencyCode;
-  minorUnit: number;
-  name: String;
-  symbol: String;
+  exponent: number;
+  name: string;
+  symbol: string;
+  introductionYear: number;
+  isCrypto: boolean;
+  countryOfOrigin?: CountryCode;
+  conversionRates: ConversionRates;
 }
 
 const BHD: Currency = {
   alphabeticCode: CurrencyCode.BHD,
-  minorUnit: 3,
+  exponent: 3,
   name: 'Dīnār Baḥrēnī',
   symbol: 'BD',
+  isCrypto: false,
+  countryOfOrigin: CountryCode.BH,
+  introductionYear: 1965,
+  conversionRates: {
+    BHD: 1.0,
+    EUR: 2.2772404,
+    GBP: 1.9637162,
+    JPY: 296.49973,
+    NOK: 22.986093,
+    USD: 2.6595745,
+  },
 };
 const EUR: Currency = {
   alphabeticCode: CurrencyCode.EUR,
-  minorUnit: 2,
+  exponent: 2,
   name: 'Euro',
   symbol: '€',
+  isCrypto: false,
+  introductionYear: 1999,
+  conversionRates: {
+    BHD: 0.43911894,
+    EUR: 1.0,
+    GBP: 0.86227807,
+    JPY: 130.21196,
+    NOK: 10.092985,
+    USD: 1.1677356,
+  },
 };
 const GBP: Currency = {
   alphabeticCode: CurrencyCode.GBP,
-  minorUnit: 2,
+  exponent: 2,
   name: 'Pound sterling',
   symbol: '£',
+  isCrypto: false,
+  introductionYear: 1158,
+  countryOfOrigin: CountryCode.GB_ENG,
+  conversionRates: {
+    BHD: 0.50922622,
+    EUR: 1.1597614,
+    GBP: 1.0,
+    JPY: 151.03931,
+    NOK: 11.69829,
+    USD: 1.3540908,
+  },
+};
+const JPY: Currency = {
+  alphabeticCode: CurrencyCode.JPY,
+  exponent: 0,
+  name: 'Japanese Yen',
+  symbol: '¥',
+  isCrypto: false,
+  introductionYear: 1869,
+  countryOfOrigin: CountryCode.JP,
+  conversionRates: {
+    BHD: 0.0033708646,
+    EUR: 0.0076778078,
+    GBP: 0.0066208062,
+    JPY: 1.0,
+    NOK: 0.077451302,
+    USD: 0.0089650654,
+  },
 };
 const NOK: Currency = {
   alphabeticCode: CurrencyCode.NOK,
-  minorUnit: 2,
+  exponent: 2,
   name: 'Norwegian krone',
   symbol: 'kr',
+  isCrypto: false,
+  introductionYear: 1875,
+  countryOfOrigin: CountryCode.NO,
+  conversionRates: {
+    BHD: 0.043522375,
+    EUR: 0.099138197,
+    GBP: 0.08550077,
+    JPY: 0.08550077,
+    NOK: 1.0,
+    USD: 0.11576246,
+  },
 };
 const USD: Currency = {
   alphabeticCode: CurrencyCode.USD,
-  minorUnit: 2,
+  exponent: 2,
   name: 'United States Dollar',
   symbol: '$',
+  introductionYear: 1792,
+  isCrypto: false,
+  countryOfOrigin: CountryCode.US,
+  conversionRates: {
+    BHD: 0.376975,
+    EUR: 0.85639333,
+    GBP: 0.73866919,
+    JPY: 111.55723,
+    NOK: 8.6380147,
+    USD: 1.0,
+  },
 };
+
+export const toMinor = (major: number, exponent: number) =>
+  Math.round(major * Math.pow(10, exponent));
+export const toMajor = (minor: number, exponent: number) =>
+  Math.round(minor / Math.pow(10, exponent));
 
 export const currencies = [BHD, EUR, GBP, NOK, USD];
