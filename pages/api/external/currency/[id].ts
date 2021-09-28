9f61ec5e826967284d1c3202a82ff150a6c4bc47
Anders Engan <anders.engan@gmail.com>
2021-09-28 13:03:12 +0200

    Lorem file doesn't can be plain text

--- pages/api/external/currency/[id].ts
+++ pages/api/external/currency/[id].ts
@@ -1,8 +1,11 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { currencies } from '../../../../utils/currency';
-import lorem from '../../../../utils/lorem';
+import { promises as fs } from 'fs';
+import path from 'path';
 
-export default function currencyHandler(
+const loremFile = 'lorem.txt';
+
+export default async function currencyHandler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
@@ -23,6 +26,8 @@ export default function currencyHandler(
   if (currency == null) {
     return res.status(404).end(`Currency Not Found`);
   }
+
+  const lorem = await fs.readFile(path.join(process.cwd(), loremFile), 'utf-8');
 
   return res.status(200).json({ ...currency, history: lorem });
 }
