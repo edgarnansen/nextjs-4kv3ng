0ef32bd8f20f4bd7676ccfa61cafcaa917378b08
Jonas Kirkemyr <jonas.kirkemyr@sparebank1.no>
2023-01-19 08:46:47 +0100

    add page rendering a single currency

---
+++ pages/[id].tsx
@@ -0,0 +1,33 @@
+import { GetServerSideProps } from 'next';
+import styles from '../styles/Home.module.css';
+import data from './example-response.json';
+
+interface Props {
+  id: string;
+}
+
+export default function ParameterRoute({ id }: Props) {
+  return (
+    <div className={styles.container}>
+      <p>parameter: {id}</p>
+
+      <h2>Object key:value</h2>
+      {Object.entries(data).map(([key, value]) => (
+        <div>
+          <b>{key}</b>: {JSON.stringify(value)}
+        </div>
+      ))}
+
+      <h2>Raw</h2>
+      <code>{JSON.stringify(data)}</code>
+    </div>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps<Props> = async (ctx) => {
+  const id = ctx.params?.id?.toString() ?? 'unknown';
+
+  return {
+    props: { id },
+  };
+};
