07db23ea3b09016818e832e1d8ba5cf4b3c6681a
Jonas Kirkemyr <jonas.kirkemyr@sparebank1.no>
2023-02-23 15:26:56 +0100

    dummy implementation for currency converter

--- pages/api/integration/currency/convert.ts
+++ pages/api/integration/currency/convert.ts
@@ -8,6 +8,18 @@ export default async function convertCurrencyHandler(
     query: { amount, from, to },
     method,
   } = req;
+  // TODO: Fetch from external API to get conversion rates
+  const PLACEHOLDER_NOK_CONVERSION_RATES = { BHD: 1, EUR: 1, GBP: 1 };
 
-  res.status(501).end('Not implemented');
+  if (to !== 'NOK') {
+    return res.status(501).end('Not implemented');
+  }
+
+  return res.status(200).json({
+    value:
+      Number(amount) *
+      PLACEHOLDER_NOK_CONVERSION_RATES[
+        from as keyof typeof PLACEHOLDER_NOK_CONVERSION_RATES
+      ],
+  });
 }
