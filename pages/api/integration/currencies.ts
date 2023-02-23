a7af25b4fc975cb6eee11069b7119dc114277a24
Jonas Kirkemyr <jonas.kirkemyr@sparebank1.no>
2023-02-23 14:27:09 +0100

    remove unused imports

--- pages/api/integration/currencies.ts
+++ pages/api/integration/currencies.ts
@@ -1,8 +1,6 @@
 import axios from 'axios';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { externalAPIBase } from '../../../utils/api';
-import https from 'https';
-import crypto from 'crypto';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   // TODO: Fetch from external API to get more currencies.
